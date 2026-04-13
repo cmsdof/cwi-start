1b88e667e8316981c3f4090c52ee8942103e8d07
Carlos Severo <carlosmseverof@gmail.com>
2026-04-13 18:27:11 -0300

    Variables and Data Types

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+    console.log ("Hello World");
+
+    const firstName = "Carlos";
+    const lastName = "Severo";
+    console.log(`My name is ${firstName} ${lastName}!`);
+    
+    let names = "John, Mary, Bob";
+    console.log(names.toUpperCase());
+
+    const number = 5;
+    console.log(number * 5);
+
+    const x = null;
+    const y = undefined;
+
+    const array = [1, 2, 3, 4, 5];
+    console.log(array);
