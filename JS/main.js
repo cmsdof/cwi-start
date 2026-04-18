6c8cf902b037889dc7db858c1aba2951d9944d96
Carlos Severo <carlosmseverof@gmail.com>
2026-04-18 16:50:59 -0300

    reformatting

--- JS/main.js
+++ JS/main.js
@@ -51,3 +51,21 @@
     console.log(thisIsCode <= thisIsNotCode); //False
     console.log(thisIsCode && thisIsNotCode); //False
     console.log(thisIsCode || thisIsNotCode); //True
+
+    // Creating a Set from an Array
+    const myArray = [1, 1, 2, 3, 3, 4, 5, 5];
+    const mySet = new Set(myArray);
+
+    console.log(myArray);
+    console.log(mySet);
+
+
+    // Set Methods
+    mySet.add(6);
+    console.log(mySet);
+
+    mySet.add({name: 'John', age: 30});
+    console.log(mySet);
+
+    mySet.delete({name: 'John', age: 30});
+    console.log(mySet);
