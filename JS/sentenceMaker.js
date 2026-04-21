24a4e473972a51969db6f7b6f591e5b802a4ed92
Carlos Severo <carlosmseverof@gmail.com>
2026-04-21 18:13:20 -0300

    freeCodeCamp exercise

---
+++ JS/sentenceMaker.js
@@ -0,0 +1,16 @@
+let adjective = "hungry", adjective2 = "clean";
+let noun = "boy", noun2 = "noodles";
+let verb = "lit", place = "city";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+
+adjective = "very hungry", adjective2 = "strong";
+noun = "boy", noun2 = "cake";
+verb = "fire", place = "capital";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
