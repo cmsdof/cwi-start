bc096f6c438d396583c5d8f54dcb598de9124fc8
Carlos Severo <carlosmseverof@gmail.com>
2026-04-22 17:39:24 -0300

    freeCodeCamp

---
+++ JS/teacherChatBot.js
@@ -0,0 +1,21 @@
+console.log("Hi there!");
+
+let botName = "teacherBot";
+
+let greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+let subject = "JavaScript";
+let topic = "strings";
+
+let sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence)
+
+let strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+
+console.log(subject.length);
+
+console.log(`Here is an example of using the length property on the word ${topic}.`)
+console.log(topic.length);
+
