832454b16f0d2bedb018f4ead0c27b22c6574a54
Carlos Severo <carlosmseverof@gmail.com>
2026-04-22 17:52:02 -0300

    complete

--- JS/teacherChatBot.js
+++ JS/teacherChatBot.js
@@ -19,3 +19,25 @@ console.log(subject.length);
 console.log(`Here is an example of using the length property on the word ${topic}.`)
 console.log(topic.length);
 
+console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+
+console.log(subject[0]);
+
+console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
+console.log(subject[1])
+
+console.log(`Here is an example of accessing the last letter in the word ${subject}`);
+
+let lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+let learningIsFunSentence = "Learning is fun.";
+
+console.log("Here are examples of finding the positions of substrings in the sentence.");
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
